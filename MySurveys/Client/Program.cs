abc803dffb3aa0489f1245f08a85010fc61ce22b
Dominik Witkowski <domwitmax@gmail.com>
2024-12-04 20:06:02 +0100

    Add OneLineQuestion and MultipleLinesQuestion

--- MySurveys/Client/Program.cs
+++ MySurveys/Client/Program.cs
@@ -8,7 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("MySurveys.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
-
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("MySurveys.ServerAPI"));
 
