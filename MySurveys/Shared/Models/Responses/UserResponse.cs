4bc8e65dca3ec81e7167c18f173bb1dabc89fcda
Dominik Witkowski <domwitmax@gmail.com>
2025-01-06 20:09:35 +0100

    Create UserController

--- MySurveys/Shared/Models/Responses/UserResponse.cs
+++ MySurveys/Shared/Models/Responses/UserResponse.cs
@@ -2,6 +2,6 @@
 
 public class UserResponse
 {
-    public string UserId { get; set; }
-    public string UserName { get; set; }
+    public string? UserId { get; set; }
+    public string? UserName { get; set; }
 }
