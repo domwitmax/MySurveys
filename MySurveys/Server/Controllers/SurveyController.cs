5e12e49ea5803670408340210331edc3e237f191
Dominik Witkowski <domwitmax@gmail.com>
2025-01-20 17:50:42 +0100

    Add SurveryController.

--- MySurveys/Server/Controllers/SurveyController.cs
+++ MySurveys/Server/Controllers/SurveyController.cs
@@ -46,5 +46,37 @@ namespace MySurveys.Server.Controllers
                 return BadRequest((Survey?)null);
             return Ok(result);
         }
+        [HttpPut("Update")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public async Task<IActionResult> UpdateSurvey([FromBody] Survey survey)
+        {
+            string? userName = User.FindFirstValue(ClaimTypes.Name);
+            if (userName is null)
+                return Unauthorized(null);
+            if (surveyService.CheckSurvey(survey))
+                return BadRequest((int?)null);
+            bool isSuccess = await surveyService.UpdateSurvey(survey, userName);
+            if (!isSuccess)
+                return BadRequest((int?)null);
+            return Ok();
+        }
+        [HttpDelete("Delete/{surveyId:int}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public async Task<IActionResult> DeleteSurvey(int surveyId)
+        {
+            string? userName = User.FindFirstValue(ClaimTypes.Name);
+            if (userName is null)
+                return Unauthorized(null);
+            if (surveyId < 1)
+                return BadRequest((int?)null);
+            bool isSuccess = await surveyService.DeleteSurvey(surveyId, userName);
+            if(!isSuccess)
+                return BadRequest((int?)null);
+            return Ok();
+        }
     }
 }
